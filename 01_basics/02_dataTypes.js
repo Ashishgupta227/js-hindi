810efe622e5487041a2676a364884693a84ab400
Ashish Gupta <112555808+Ashishgupta227@users.noreply.github.com>
2025-02-04 16:11:16 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+"use strict";
+// alert( 3+3 ) //we are using node.js not browser 
+console.log(3+3);
+console.log("Ashish")
+
+let name = "Ashish"
+let age = 21
+let isLoggedIn = false
+
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
