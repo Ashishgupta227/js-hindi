1a66d28b9192ee2581fc050e91d4a5a83751dc6c
Ashish Gupta <112555808+Ashishgupta227@users.noreply.github.com>
2025-02-04 16:13:00 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "ashish@google.com"
+var accountPassword = "12345"
+accountCity = "Patna"
+let accountState;
+
+//accountId = 2
+accountEmail = "as@as.com"
+accountPassword = "121212"
+accountCity = "Delhi"
+
+console.log(accountId);
+console.table([accountId,accountEmail, accountPassword, accountCity,accountState])
