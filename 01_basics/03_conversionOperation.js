37a715636244a5266d5facee3d6e12ea481ec302
Ashish Gupta <112555808+Ashishgupta227@users.noreply.github.com>
2025-02-05 08:29:31 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,56 @@
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber);
+
+// OPERATION
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 = " Ashish"
+
+let str3 = str1+str2
+//console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+3) //consider hole as string
+// console.log(1+2+"3") //first 1+2 then 33
+
+// console.log( (3+4)* 5 % 3)
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+//console.log(num1,num2,num3)
+
+let gameCounter = 100
+//gameCounter++;
+++gameCounter
+//console.log(gameCounter)
+
+let x = 3
+x++
+//console.log(x)
+
+let y = 3
+++y
+//console.log(y)
+
+let a =5
+b = a++
+console.log(a,b)
+
+let c = 4
+d = ++c
+console.log(c,d)
